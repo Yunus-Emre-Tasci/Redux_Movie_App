20c352569945f62bbddb807b3c1ca8b3d1ff76b7
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-06-08 23:57:35 +0300

    selector

--- src/components/page/MoviesPage.jsx
+++ src/components/page/MoviesPage.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import { useSelector } from "react-redux"
 
 const MoviesPage = () => {
+const { movies } = useSelector;((state)=>state.movies)
+
   return (
     <div>MoviesPage</div>
   )
